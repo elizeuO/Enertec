c94c94ac25039c20ddeffa1fb2d24b6a9b12487c
Elizeu <elizeu_os@hotmail.com>
2023-02-13 10:54:38 -0300

    Add Section Instagram

---
+++ themes/enertec/partials/section-instagram.php
@@ -0,0 +1,9 @@
+<div class="c-section c-section--gray">
+    <div class="c__container">
+        <h2 class="c-title c-title--decoration c__center">
+            Nosso instagram
+        </h2>
+
+        <?= do_shortcode('[elfsight_instagram_feed id="1"]')?>
+    </div>
+</div>
