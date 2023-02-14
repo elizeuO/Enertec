c04bc52269261dc3528440161d7946713b48456f
Elizeu <elizeu_os@hotmail.com>
2023-02-14 11:22:37 -0300

    Add active functionality to anchor links

--- themes/enertec/dist/js/enertec.js
+++ themes/enertec/dist/js/enertec.js
@@ -1,6 +1,6 @@
-// When the user scrolls the page, execute toggleHeaderSticky
 window.onscroll = function () {
   toggleHeaderSticky();
+  toggleActiveLink();
 };
 
 function toggleHeaderSticky() {
@@ -11,5 +11,34 @@ function toggleHeaderSticky() {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+  }
+}
+
+function toggleActiveLink() {
+  // Get all the links with an ID that starts with 'section'
+  const links = document.querySelectorAll('header nav a[href^="#"]');
+  if (null === links) {
+    return;
+  }
+
+  // Get the current scroll position of the window
+  const currentScrollPos = window.scrollY + 100;
+
+  // Loop through all the links
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+
+    // Get the section that the link points to
+    const section = document.querySelector(link.hash);
+
+    // If the section is in the viewport, add an 'active' class to the link
+    if (
+      section.offsetTop <= currentScrollPos &&
+      section.offsetTop + section.offsetHeight > currentScrollPos
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
   }
 }
